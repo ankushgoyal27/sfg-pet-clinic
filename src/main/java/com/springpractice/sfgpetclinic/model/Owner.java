6785cdbab5595adc30f4382a2b1cee313364fbe1
Ankush <ankushgoyal11@gmail.com>
2019-05-30 19:20:33 -0400

    POJO Classes

---
+++ src/main/java/com/springpractice/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springpractice.sfgpetclinic.model;
+
+public class Owner extends  Person {
+}
---
+++ src/main/java/com/springpractice/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.springpractice.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
