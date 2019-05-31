8561c9c7094601732cb971ac4087f930cc9d4663
Ankush <ankushgoyal11@gmail.com>
2019-05-30 23:27:08 -0400

    Refactor to Multi Module maven project. Closes #3

--- pet-clinic-data/src/main/java/com/springpractice/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springpractice/sfgpetclinic/model/Owner.java
@@ -1,4 +1,4 @@
 package com.springpractice.sfgpetclinic.model;
 
-public class Owner extends  Person {
+public class Owner extends Person {
 }
--- pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package com.springpractice.sfgpetclinic;
+package sfgpetclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
