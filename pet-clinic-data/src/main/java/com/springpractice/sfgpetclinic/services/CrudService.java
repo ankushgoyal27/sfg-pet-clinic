282ec23f002e4d193b9831c6d87f0a410d6e8d45
Ankush <ankushgoyal11@gmail.com>
2019-06-26 14:31:13 -0400

    closes #7

---
+++ pet-clinic-data/src/main/java/com/springpractice/sfgpetclinic/services/CrudService.java
@@ -0,0 +1,16 @@
+package com.springpractice.sfgpetclinic.services;
+
+import java.util.Set;
+
+//Common Repo
+
+public interface CrudService<T, ID> {
+
+	Set<T> findAll();
+	
+	T findById(ID id);
+	
+	T save(T object); 
+	
+	void delete(T object);
+}
